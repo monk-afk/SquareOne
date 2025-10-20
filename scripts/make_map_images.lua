cb48ea01a5fb4a405ff4764557913fd0b1cd5d52
monk <admin@monk.moe>
2025-10-20 14:03:38 -0300

    tidy code in mapper script, increase image generation from 6+1 to 8+1 columns

--- scripts/make_map_images.lua
+++ scripts/make_map_images.lua
@@ -1,25 +1,35 @@
+--[[
+ Requirements:
+   - minetestmapper
+   - colors.txt
+
+ To use:
+ - Build minetestmapper binary from the GitHub repo:
+       https://github.com/luanti-org/minetestmapper
+
+ - In minetestmapper directory, locate the file: util/dumpnodes/init.lua
+   - There is a chat command, add it to a mod and use it in-game.
+   - Locate the nodes.txt file created in worldpath.
+
+ - In minetestmapper directory, locate the file: util/generate_colorstxt.py, Run with args:
+   $ ./util/generate_colorstxt.py -g /full/path/worlds/world -m /full/path/worlds/world /full/path/worlds/world/nodes.txt
+
+   - Open this file for editing and configure the local variables:
+       local server = "world of your server"
+       local images = "where the images will be placed"
+       * TODO: local max_y, min_y = max height, min height of captured image
+]]
+
 local function help_message()
   io.write([[ Generate Luanti(Minetest) Map Images
-  Requirements:
-    - minetestmapper binary
-    - colors.txt
-
-  To use:
-    - Open this file for editing and configure the local variables:
-        local server = "world of your server"
-        local images = "where the images will be placed"
-        * TODO: local max_y, min_y = max height, min height of captured image
-
-    - Run from command line:
-
-      $ lua mapper.lua [params]
-      $ lua mapper.lua x=1 z=-1 d=256
+  Command usage:
+      $ lua mapper.lua [param] [param]
 
   Accepted parameters:
-      grid    Automates image creation using a grid with each image being 4096px with 0,0 being the center
-      x=[N]   Focus point of coordinate on X axis
-      z=[N]   Focus point of coordinate on Z axis
-      d=[N]   Optional Dimention of the output image in pixels (Default 16)
+      g | grid  Generate images in a grid layout with each image being 4096px
+      x=[N]     Focus point of coordinate on X axis
+      z=[N]     Focus point of coordinate on Z axis
+      d=[N]     Optional Dimention of the output image in pixels (Default 16)
 ]]); io.stdout:flush()
 end
 
@@ -29,11 +39,11 @@ local server = "${HOME}/squareone/worlds/world/"
 local images = "${PWD}/generated_images/" .. today .. "/"
 os.execute("mkdir -p " .. images)
 
-local option = "--backend postgresql --noemptyimage --colors colors.txt --max-y 120 --min-y -12"
+local option = "--backend postgresql --noemptyimage --colors colors.txt --max-y 120 --min-y -36"
 local exec_string = "${PWD}/minetestmapper %s -i %s -o %ssquareone_x%s_z%s.png --geometry %s:%s+%s+%s;"
 
 local function generate_image_at(x, z, d)
-  local a = math.floor(d / 2)
+  local a = d // 2
   local ax, az = x+(-a), z+(-a)
   local exec_format = string.format(
       exec_string, option, server, images, x, z, ax, az, d, d
@@ -44,7 +54,7 @@ end
 
 local function process_grid()
   local d = 4096
-  local grid = 6 -- this must be multiples of 2
+  local grid = 8 -- this must be multiples of 2, gives (grid + 1) columns and rows
   local max_pos = (d * grid) // 2
   for x = -max_pos, max_pos, d do
     for z = -max_pos, max_pos, d do
@@ -53,11 +63,12 @@ local function process_grid()
     end
   end
   os.execute("echo " .. today .. " > " .. images .. "last_update.txt")
+  io.write("Done! Generated Images in: ", images, "\n"); io.stdout:flush()
 end
 
 
 local function process_command(arg)
-  if arg[1] == "grid" then
+  if arg[1] == "grid" or arg[1] == "g" then
     return process_grid()
   end
 
@@ -70,18 +81,20 @@ local function process_command(arg)
       end)
   end
 
-  local M = 30720
-  local d = math.max(math.min(M, tonumber(cmd.d) or 256), 16)
+  local M = 30720 -- the actual map limit size
+  local d = math.max(math.min(M, tonumber(cmd.d) or 256), 16) -- this needs to be re-considered
   local x = tonumber(cmd.x)
   local z = tonumber(cmd.z)
   local y = tonumber(cmd.y)
 
   if not (x and z and d) or cmd.help or cmd.h then
     return help_message()
+
   elseif x < -M or z < -M
       or x >  M or z >  M then
     io.write("Please use coordinates between ", -M, " and ", M, ".\n"); io.stdout:flush()
     return
+
   else
     io.write("Generating Image at Center Coordinates: (", x, ", ", z, ")\n"); io.stdout:flush()
     generate_image_at(x, z, d)
@@ -89,23 +102,10 @@ local function process_command(arg)
 end
 
 process_command(arg)
-
-
--- - Build minetestmapper binary from the GitHub repo:
---       https://github.com/luanti-org/minetestmapper
-
--- - In minetestmapper directory, locate the file: util/dumpnodes/init.lua
---   - There is a chat command, add it to a mod and use it in-game.
---   - Locate the nodes.txt file created in worldpath.
-
--- - In minetestmapper directory, locate the file: util/generate_colorstxt.py, Run with args:
---   $ ./util/generate_colorstxt.py -g /full/path/worlds/world -m /full/path/worlds/world /full/path/worlds/world/nodes.txt
-
-
 ------------------------------------------------------------------------------------
 -- MIT License                                                                    --
 --                                                                                --
--- Copyright © 2025 monk (Discord ID: 699370563235479624)                         --
+-- Copyright © 2025 monk https://github.com/monk-afk/                             --
 --                                                                                --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy   --
 -- of this software and associated documentation files (the "Software"), to deal  --
